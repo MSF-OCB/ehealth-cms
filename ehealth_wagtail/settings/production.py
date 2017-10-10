b477cfd7096431be2f3c1517514463a0cae0ebde
Pierre-Louis Mercereau <pilou@MBPZ35.local>
2017-10-10 21:01:23 +0200

    database connection

--- ehealth_wagtail/settings/production.py
+++ ehealth_wagtail/settings/production.py
@@ -11,5 +11,9 @@ except ImportError:
 
 from os import environ
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
