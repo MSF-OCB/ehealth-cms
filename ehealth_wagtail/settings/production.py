3368c2aadc374f1f54f70b3573d0f61e76fe31fd
Pierre-Louis Mercereau <pilou@MBPZ35.local>
2017-10-10 21:03:09 +0200

    heroku set up of postGIS

--- ehealth_wagtail/settings/production.py
+++ ehealth_wagtail/settings/production.py
@@ -14,6 +14,7 @@ from os import environ
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
